925a831c8788d4b369704f1c0b1fa14db795af89
Lavitz <53936803+Lavitzio@users.noreply.github.com>
2019-08-09 20:23:17 -0400

    Update LavitzFrostboltzWA.lua

--- LavitzFrostboltzWA.lua
+++ LavitzFrostboltzWA.lua
@@ -1,5 +1,6 @@
 function()
 
+-- Function to get cost of spell
 aura_env.GetCost = function(spellID, powerType)
     if not spellID then return end
     powerType = powerType or UnitPowerType("player")
@@ -13,17 +14,19 @@ aura_env.GetCost = function(spellID, powerType)
 end
 
 local mana = UnitPower("player", 0)
-local spellRankIdsSize = 14
-local spellRankIds = {133, 116, 143, 205, 145, 837, 7322, 8406, 8407, 8408, 10179, 10180, 10181, 25304}
 local cost = 0
 
-for i=1,spellRankIdsSize do
-  if IsPlayerSpell(spellRankIds[i]) then
-    cost = aura_env.GetCost(spellRankIds[i])
+-- Spell IDs of various ranks of Fireball and Frostbolt (Fire1, Frost1, Fire2, Frost2, Fire3, Frost3 +)
+local spellIds = {133, 116, 143, 205, 145, 837, 7322, 8406, 8407, 8408, 10179, 10180, 10181, 25304}
+local spellIdsSize = 14
+
+for i=1,spellIdsSize do
+  if IsPlayerSpell(spellIds[i]) then
+    cost = aura_env.GetCost(spellIds[i])
   end
 end
 
-local count = math.floor(mana / cost)
-return "Can cast  " .. string.format("%.f", count);
+local num = math.floor(mana / cost)
+return "Can cast " .. string.format("%.f", num)
 
 end
