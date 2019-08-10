32241d659562f0cf5f3e87522f1406779ae765f0
Lavitz <53936803+Lavitzio@users.noreply.github.com>
2019-08-09 20:23:31 -0400

    Update LavitzFrostboltzIconFireWA.lua

--- LavitzFrostboltzIconFireWA.lua
+++ LavitzFrostboltzIconFireWA.lua
@@ -1,11 +1,1 @@
-function()
-  
-local showFireball = false
-
-if (IsPlayerSpell(133) and not IsPlayerSpell(116)) or (IsPlayerSpell(143) and not IsPlayerSpell(205)) or (IsPlayerSpell(145) and not IsPlayerSpell(837)) then
-  showFireball = true
-end
-
-return showFireball
-
-end
+function() return (IsPlayerSpell(133) and not IsPlayerSpell(116)) or (IsPlayerSpell(143) and not IsPlayerSpell(205)) or (IsPlayerSpell(145) and not IsPlayerSpell(837)) end
