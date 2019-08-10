acd67ed2b6b223de82dee236050b13a0a1875ef3
Lavitz <53936803+Lavitzio@users.noreply.github.com>
2019-08-09 20:23:06 -0400

    Update LavitzFrostboltzHelpWA.lua

--- LavitzFrostboltzHelpWA.lua
+++ LavitzFrostboltzHelpWA.lua
@@ -1,15 +1,17 @@
 function()
 
 local level = UnitLevel("player")
-local levelBreaksSize = 13
+-- Levels at which you get the next best spell rank
 local levelBreaks = {4, 6, 8, 12, 14, 20, 26, 32, 38, 44, 50, 56, 60}
-local spellRankIds = {116, 143, 205, 145, 837, 7322, 8406, 8407, 8408, 10179, 10180, 10181, 25304}
-
+local levelBreaksSize = 13
+-- Spell IDs of various ranks of Fireball and Frostbolt (Fire1, Frost1, Fire2, Frost2, Fire3, Frost3 +)
+local spellIds = {116, 143, 205, 145, 837, 7322, 8406, 8407, 8408, 10179, 10180, 10181, 25304}
 local spellIdToLearn = -1
 
+
 for i=levelBreaksSize,1,-1 do
-  if (level >= levelBreaks[i] and not IsPlayerSpell(spellRankIds[i])) then
-    spellIdToLearn = spellRankIds[i]
+  if (level >= levelBreaks[i] and not IsPlayerSpell(spellIds[i])) then
+    spellIdToLearn = spellIds[i]
     break
   end
 end
