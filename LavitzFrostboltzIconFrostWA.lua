d02f30ae37de31ffb00b07e15207560a2824e7a4
Lavitz <53936803+Lavitzio@users.noreply.github.com>
2019-08-09 20:23:57 -0400

    Update LavitzFrostboltzIconFrostWA.lua

--- LavitzFrostboltzIconFrostWA.lua
+++ LavitzFrostboltzIconFrostWA.lua
@@ -1,11 +1,1 @@
-function()
-  
-local showFrostbolt = false
-
-if (IsPlayerSpell(116) and not IsPlayerSpell(143)) or (IsPlayerSpell(205) and not IsPlayerSpell(145)) or (IsPlayerSpell(837)) then
-  showFrostbolt = true
-end
-
-return showFrostbolt
-
-end
+function() return (IsPlayerSpell(116) and not IsPlayerSpell(143)) or (IsPlayerSpell(205) and not IsPlayerSpell(145)) or (IsPlayerSpell(837)) end
