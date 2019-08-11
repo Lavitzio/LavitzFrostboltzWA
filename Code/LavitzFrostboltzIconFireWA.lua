b502ea8e1f391e007313aa1d9e0db3b3a58850fa
Lavitz <53936803+Lavitzio@users.noreply.github.com>
2019-08-11 15:14:36 -0400

    Update LavitzFrostboltzIconFireWA.lua

--- Code/LavitzFrostboltzIconFireWA.lua
+++ Code/LavitzFrostboltzIconFireWA.lua
@@ -1,1 +1,16 @@
-function() return (IsPlayerSpell(133) and not IsPlayerSpell(116)) or (IsPlayerSpell(143) and not IsPlayerSpell(205)) or (IsPlayerSpell(145) and not IsPlayerSpell(837)) end
+function()
+
+  local level = UnitLevel("player")
+  local show = false
+
+  if level >= 12 and not IsPlayerSpell(837) then
+    show = true
+  elseif level >= 6 and not IsPlayerSpell(205) then
+    show = true
+  elseif level >= 1 and not IsPlayerSpell(116) then
+    show = true
+  end
+
+  return show
+
+end
